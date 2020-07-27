4cc2206fa74c14ec75044836c5d1bc534c359f88
Teuchezh Nurbiy <40827089+Cherkes001@users.noreply.github.com>
2020-07-27 21:24:21 +0300

    add window switch (ALT+TAB)
    Maybe Bugs this func

--- ultimate_knob/ultimate_knob.ino
+++ ultimate_knob/ultimate_knob.ino
@@ -1,38 +1,49 @@
+/////////////////////////////////////////////////////////
 #define CLK 4
 #define DT 5
 #define SW 6
 #include "HID-Project.h"
 #include <GyverEncoder.h>
 Encoder enc(CLK, DT, SW);
-
-const int pinLed = LED_BUILTIN;
-#include <GyverButton.h>
-#define BTN_PIN 7
-GButton lshift(BTN_PIN);
-
+////////////////////////////////////////////////////////
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
+///////////////////////////////////////////////////////
+#include "GyverTimer.h"
+GTimer hiTimer(MS);
+///////////////////////////////////////////////////////
+const int pinLed = LED_BUILTIN;
+#include <GyverButton.h>
+#define BTN_PIN 7
+GButton lshift(BTN_PIN);
+///////////////////////////////////////////////////////
 void setup()
 {
   enc.setType(TYPE2);
   enc.setTickMode(AUTO);
-  pinMode(pinLed, OUTPUT);
   Keyboard.begin();
   System.begin();
   Serial.begin(9600);
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(WHITE);
+  display.clearDisplay();
+
+  hiTimer.setTimeout(3000);
+  hello();
 }
 
 void loop()
 {
-  lshift.tick();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
 
+  lshift.tick();
   if (lshift.isHold())
   {
     prog2();
@@ -40,17 +51,25 @@ void loop()
   else
   {
     prog1();
-    display.clearDisplay();
   }
+}
 
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
+void hello()
+{
+  hiTimer.start();
+  display.println("Ultimate Knob");
+  display.display();
+
+  if (hiTimer.isReady())
+  {
+    display.clearDisplay();
+    Serial.println("Таймер сработал");
+  }
 }
 
 void prog1()
 {
+  display.clearDisplay();
   Serial.println("Программа 1 выполняется");
   if (enc.isClick())
   {
@@ -58,33 +77,65 @@ void prog1()
     display.println("PLAY/PAUSE");
     display.display();
   }
-  if (enc.isDouble())
+  else if (enc.isDouble())
   {
     Consumer.write(MEDIA_NEXT);
     display.println("NEXT");
     display.display();
   }
-  if (enc.isHolded())
+  else if (enc.isHolded())
   {
     Consumer.write(MEDIA_VOL_MUTE);
     display.println("MUTE");
     display.display();
   }
-  if (enc.isRight())
+  else if (enc.isRight())
   {
     Consumer.write(MEDIA_VOL_UP);
     display.println("VOL UP");
     display.display();
   }
-  if (enc.isLeft())
+  else if (enc.isLeft())
   {
     Consumer.write(MEDIA_VOL_DOWN);
     display.println("VOL DOWN");
     display.display();
   }
+  else
+  {
+  }
 }
 
 void prog2()
 {
+  display.clearDisplay();
   Serial.println("Программа 2 выполняется");
+  if (enc.isDouble())
+  {
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_TAB);
+    Keyboard.release(KEY_TAB);
+    display.println("ALT+TAB");
+    display.display();
+  }
+  else if (enc.isRight())
+  {
+    Keyboard.press(KEY_RIGHT_ARROW);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    display.println("RIGHT ARROW");
+    display.display();
+  }
+  else if (enc.isLeft())
+  {
+    Keyboard.press(KEY_LEFT_ARROW);
+    Keyboard.release(KEY_LEFT_ARROW);
+    display.println("LEFT ARROW");
+    display.display();
+  }
+  else if (enc.isClick())
+  {
+    Keyboard.releaseAll();
+    display.println("RELEASE ALL");
+    display.display();
+  }
 }
